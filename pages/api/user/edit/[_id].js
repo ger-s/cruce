cddb2a85f9c8df76fe62b6686521db4d14d784b5
ger <ger@pop-os.localdomain>
2021-11-29 19:24:40 -0300

    paginas login y register

--- pages/api/user/edit/[_id].js
+++ pages/api/user/edit/[_id].js
@@ -1,6 +1,7 @@
 import dbConnect from "../../../../utils/dbConnect";
 import User from "../../../../models/User";
 
+
 dbConnect();
 
 export default async (req, res) => {
@@ -9,7 +10,21 @@ export default async (req, res) => {
   switch (method) {
     case "PUT":
       try {
-        const userModified = await User.findOneAndUpdate({_id: `${req.query._id}`}, req.body)
+        // req.body.passConfirmation va a ser una key que se mete desde el front en el req.body
+        // sería el input del "ingrese su contraseña actual"
+        if (req.body.passConfirmation) {
+          // en el localsotrage se guarda el user desde jwt, busco al usuario real desde esa id
+          const userFound = await User.findOne({ _id: localstorage.getItem("user").id });
+          // se comparan la supuesta contraseña actual, con la real traída con el findOne (hasheada)
+          if (await User.comparePassword(req.body.passConfirmation, userFound.password)) {
+            // si todo sale bien, se updatea el usuario
+            const userModified = await User.findOneAndUpdate({_id: `${req.user._id}`}, {password: req.body.newPassword})
+          } else {
+            res.status(400).json({ success: [false, "la contraseña ingresada es incorrecta."] });
+          }
+        } else {
+          const userModified = await User.findOneAndUpdate({_id: `${req.query._id}`}, req.body)
+        }
         return res.status(201).json({ success: true, data: "Usuario modificado satisfactoriamente." });
         
       } catch (error) {
--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,33 @@
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import Link from 'next/link'
+import { Container } from 'semantic-ui-react'
+
 const Login = () => {
   return (
-    <h1>
-      acá va a ir el login
-    </h1>
+    <Container>
+    
+    <Container
+      textAlign='center'
+      style={{marginTop: "20%"}}
+      
+      >
+    <Form>
+    <Form.Field>
+      <label><h3>Email</h3></label>
+      <input placeholder='Email' style={{width: "75%"}} />
+    </Form.Field>
+    <Form.Field>
+      <label><h3>Contraseña</h3></label>
+      <input placeholder='Contraseña'  style={{width: "75%"}}/>
+    </Form.Field>
+    <Form.Field>
+    <p><Link href="#">Olvidé mi contraseña</Link>.</p>
+    </Form.Field>
+    <Button type='submit'>Enviar</Button>
+  </Form>
+  <p>¿No tenés cuenta? <Link href="/register">registrate</Link>.</p>
+  </Container>
+  </Container>
   )
 }
 
