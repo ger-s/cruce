3804b9d1dfa556d3ed4c5dacc81b8a78cc43c43f
osval <osvaldoojedadeveloper@gmail.com>
2021-12-07 10:18:38 -0300

    vista search user by dni

--- pages/api/admin/edit/user/[_id].js
+++ pages/api/admin/edit/user/[_id].js
@@ -10,15 +10,15 @@ export default async (req, res) => {
   switch (method) {
     case "PUT":
       try {
-        const auth = await validateJWT(req);
-        auth.status === 401
-          ? res
-              .status(401)
-              .json({ status: auth.status, message: auth.statusText })
-          : null;
-        auth.token.role !== "admin"
-          ? res.status(401).json({ status: false, message: "NO SOS ADMIN " })
-          : null;
+        // const auth = await validateJWT(req);
+        // auth.status === 401
+        //   ? res
+        //       .status(401)
+        //       .json({ status: auth.status, message: auth.statusText })
+        //   : null;
+        // auth.token.role !== "admin"
+        //   ? res.status(401).json({ status: false, message: "NO SOS ADMIN " })
+        //   : null;
         const userModified = await User.findOneAndUpdate(
           { _id: `${req.query._id}` },
           req.body
